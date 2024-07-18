ef3e01008fe4b42de080b72e927abcafe142e3c0
Jason Brunch <jasonbunce2@gmail.com>
2024-07-17 21:26:03 -0700

    Made the individual menu cards responsive

--- components/sections/MenuSection.js
+++ components/sections/MenuSection.js
@@ -9,9 +9,9 @@ const MenuSection = () => {
         <section className={styles.section}>
             <div className={styles.burgerSection}>
                 <h3 className={styles.title}>Burgers</h3>
-                <div className={`${styles.menuItem} debug`}>
-                    <div className={`${styles.menuItemTopRow} debug2`}>
-                        <h5 className="white-text">Signature Chesseburger</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="white-text no-wrap">Signature Chesseburger</h5>
                         <hr className={styles.horizontalLine} />
                         <h5 className="bright-red-text">$13.00</h5>
                     </div>
@@ -20,55 +20,132 @@ const MenuSection = () => {
                     </div>
 
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="white-text">Signature Chesseburger</h5>
-                    <h5 className="bright-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="white-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="lightest-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="white-text">Signature Chesseburger</h5>
-                    <h5 className="bright-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="white-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="lightest-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="white-text">Signature Chesseburger</h5>
-                    <h5 className="bright-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="white-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="lightest-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
             </div>
             <div className={styles.sidesSection}>
                 <h3 className={styles.title}>Sides</h3>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
             </div>
             <div className={styles.sweetsSection}>
                 <h3 className={styles.title}>Sweets</h3>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} `}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} 2`}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} 2`}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
-                <div className={styles.menuItem}>
-                    <h5 className="medium-text">Signature Chesseburger</h5>
-                    <h5 className="light-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} `}>
+                    <div className={`${styles.menuItemTopRow} 2`}>
+                        <h5 className="dark-text no-wrap">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="light-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
             </div>
         </section>
