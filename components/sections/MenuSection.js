78fbd14bb3e654a0080d4218d3cd78b5e38423ac
Jason Brunch <jasonbunce2@gmail.com>
2024-07-17 18:42:53 -0700

    Added responsiveness to menu

--- components/sections/MenuSection.js
+++ components/sections/MenuSection.js
@@ -9,9 +9,16 @@ const MenuSection = () => {
         <section className={styles.section}>
             <div className={styles.burgerSection}>
                 <h3 className={styles.title}>Burgers</h3>
-                <div className={styles.menuItem}>
-                    <h5 className="white-text">Signature Chesseburger</h5>
-                    <h5 className="bright-red-text">$13.00</h5>
+                <div className={`${styles.menuItem} debug`}>
+                    <div className={`${styles.menuItemTopRow} debug2`}>
+                        <h5 className="white-text">Signature Chesseburger</h5>
+                        <hr className={styles.horizontalLine} />
+                        <h5 className="bright-red-text">$13.00</h5>
+                    </div>
+                    <div className={styles.menuItemBottomRow}>
+                        <p className="lightest-text">Loprem ipsum dolor sit amet, consecttetur adipiscing elit.</p>
+                    </div>
+
                 </div>
                 <div className={styles.menuItem}>
                     <h5 className="white-text">Signature Chesseburger</h5>
